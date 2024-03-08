f56eeb2ff7a62e5fb54232783abad3a750386382
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-07 19:04:15 -0600

    feat: Add state management and API call functionality

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, computed } from 'vue';
 
-const count = ref(0);
+const states = reactive({
+  username: '',
+  password: '',
+  loading: false,
+  showPassword: false,
+  result: '',
+  error: '',
+})
+
+const computedPasswordOrTextType = computed(() => {
+  if (states.showPassword) return 'text'
+  return 'password';
+});
+
+function showPassword() {
+  states.showPassword = !states.showPassword
+}
+
+async function cleanWatchList() {
+  try {
+    states.loading = true;
+
+    const res = await fetch('/api/clean', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: states.username,
+        password: states.password,
+      }),
+    });
+
+    const data = await res.json();
+
+    states.result = data;
+  } catch (error) {
+    states.error = error.message
+  } finally {
+    states.loading = false;
+  }
+}
+
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
-  <button @click="count++">✅</button>
-  <button @click="count--">❌</button>
+  <div>
+    <label for="username">username</label>
+    <input type="text" name="username" id="username" v-model="states.username">
+  </div>
+
+  <div>
+    <label for="password">password</label>
+    <input :type="computedPasswordOrTextType" name="password" id="password" v-model="states.password">
+    <button @click="showPassword"> {{ states.showPassword ? '🫣' : '🙈' }}</button>
+  </div>
+
+  <button @click="cleanWatchList">Clean</button>
+
+  <p v-if="states.loading">loading...</p>
+  <pre v-if="states.result">{{ states.result }}</pre>
+  <pre v-if="states.error">{{ states.error }}</pre>
 </template>
 
 <style scoped>
