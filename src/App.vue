d3062f96e7a9842a7cd637cf26873c3c414baa08
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-07 20:26:15 -0600

    style: Update styling in App.vue and tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -47,27 +47,40 @@ async function cleanWatchList() {
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="card w-[500px] bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">Plex Watchlist Cleaner</h2>
 
-  <div>
-    <label for="username">username</label>
-    <input type="text" name="username" id="username" v-model="states.username">
+      <div>
+        <label class="form-control w-full max-w-xs">
+          <div class="label">
+            <span class="label-text">Username</span>
+          </div>
+        </label>
+        <input type="text" v-model="states.username" placeholder="johnwack" class="input input-bordered w-full "
+          :disabled="states.loading" />
+      </div>
+
+      <div>
+        <label class="form-control w-full max-w-xs">
+          <div class="label">
+            <span class="label-text">Password</span>
+          </div>
+        </label>
+        <input type="text" v-model="states.password" placeholder="********" class="input input-bordered w-full "
+          :disabled="states.loading" />
+      </div>
+
+      <button class="btn btn-primary mt-2" @click="cleanWatchList" :disabled="states.loading">
+        <span v-if="states.loading" class="loading loading-spinner"></span>
+        <span v-else>Clean</span>
+      </button>
+
+      <div v-if="(!states.loading && states.result) || (!states.loading && states.error)"
+        class="overflow-scroll text-sm p-4 bg-[#1F1F1F] text-white rounded-md mt-2">
+        <pre class="text-wrap" v-if="!states.loading && states.result">{{ states.result }}</pre>
+        <pre class="text-wrap" v-if="!states.loading && states.error">{{ states.error }}</pre>
+      </div>
+    </div>
   </div>
-
-  <div>
-    <label for="password">password</label>
-    <input :type="computedPasswordOrTextType" name="password" id="password" v-model="states.password">
-    <button @click="showPassword"> {{ states.showPassword ? '🫣' : '🙈' }}</button>
-  </div>
-
-  <button @click="cleanWatchList">Clean</button>
-
-  <p v-if="states.loading">loading...</p>
-  <pre v-if="!states.loading && states.result">{{ states.result }}</pre>
-  <pre v-if="!states.loading && states.error">{{ states.error }}</pre>
 </template>
-
-<style scoped>
-</style>
